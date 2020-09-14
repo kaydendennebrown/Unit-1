cfc29017c52dec47f19bedf50c0a6191e24b826b
kaydendennebrown <825058@mystma.org>
2020-09-14 18:27:52 +0000

    Top Five Movies(Complete)

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -3,6 +3,9 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("1. I am Legend");
+        System.out.println("2. The Butterfly Affect");
+        System.out.println("3. The Blair Witch Project");
+        System.out.println("4. Joker");
+        System.out.println("5. American Sniper");
     }
-
 }
