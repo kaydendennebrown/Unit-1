1895bce75abb8a3012150d0f13fcd6c977b16a85
kaydendennebrown <825058@mystma.org>
2020-09-18 01:16:10 +0000

    September 17 Assignment

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -2,10 +2,38 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. I am Legend");
-        System.out.println("2. The Butterfly Affect");
-        System.out.println("3. The Blair Witch Project");
-        System.out.println("4. Joker");
-        System.out.println("5. American Sniper");
+boolean hasBlondeHair = false;
+byte numOfSiblings = 4;
+char lastInitial = 'D';
+short popOfStma = 17000;
+int popOfUsa = 300000000;
+long popOfEarth = 8000000;
+float firsDigOfPi = (float) 3.14159;
+double halfOfPi = 1.57079632679;
+
+System.out.println("Do I Have Blonde Hair?");
+System.out.println(hasBlondeHair);
+
+System.out.println("How Many Siblings Do I Have?");
+System.out.println(numOfSiblings);
+
+System.out.println("Whats My Last Initial?");
+System.out.println(lastInitial);
+
+System.out.println("About How Many People Live In Saint Michael?");
+System.out.println(popOfStma);
+
+System.out.println("About How Many People Live In The United States?");
+System.out.println(popOfUsa);
+
+System.out.println("About How Many People Live On Earth?");
+System.out.println(popOfEarth);
+
+System.out.println("What are the first 6 Digits Of Pi?");
+System.out.println(firsDigOfPi);
+
+System.out.println("What Is Half Of Pi?");
+System.out.println(halfOfPi);
+
     }
 }
