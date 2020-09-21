cf76501f9f25e0823c843e38cfc97f5ea1462786
kaydendennebrown <825058@mystma.org>
2020-09-21 19:51:45 +0000

    Madlib Assignment

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -2,39 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-boolean hasBlondeHair = false;
-byte numOfSiblings = 4;
-char lastInitial = 'D';
-short popOfStma = 17000;
-int popOfUsa = 300000000;
-long popOfEarth = 8000000;
-float firsDigOfPi = (float) 3.14159;
-double halfOfPi = 1.57079632679;
-
-System.out.println("Do I Have Blonde Hair?");
-System.out.println(hasBlondeHair);
-
-System.out.println("How Many Siblings Do I Have?");
-System.out.println(numOfSiblings);
-
-System.out.println("Whats My Last Initial?");
-System.out.println(lastInitial);
-
-System.out.println("About How Many People Live In Saint Michael?");
-System.out.println(popOfStma);
-
-System.out.println("About How Many People Live In The United States?");
-System.out.println(popOfUsa);
-
-System.out.println("About How Many People Live On Earth?");
-System.out.println(popOfEarth);
-
-System.out.println("What are the first 6 Digits Of Pi?");
-System.out.println(firsDigOfPi);
-
-System.out.println("What Is Half Of Pi?");
-System.out.println(halfOfPi);
-
 String person1 = "Cheif Keef";
 String person2 = "Bronny Jr";
 String verb1 = "run";
