c5d9d70c352dc6c14db120320a1014158f1ed788
kaydendennebrown <825058@mystma.org>
2020-09-21 19:50:10 +0000

    Madlib Assignment

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -35,5 +35,20 @@ System.out.println(firsDigOfPi);
 System.out.println("What Is Half Of Pi?");
 System.out.println(halfOfPi);
 
+String person1 = "Cheif Keef";
+String person2 = "Bronny Jr";
+String verb1 = "run";
+String verb2 = "jump";
+String verb3 = "swim";
+String food = "pumpkin pie";
+String dayOfWeek = "Saturday";
+String place1 = "bowling alley";
+String place2 = "Pizza Hut";
+String adjective1 = "sweaty";
+String adjective2 = "gigantic";
+
+System.out.println(person1 +" " + "is a programmer who likes to" + " " + verb1 + ".");
+System.out.println("Every"+ " " + dayOfWeek + " " + "they make a" + " " + adjective1 + " " + food + " " + "for brakfast and" + " " + verb2 + " " + "at" + " " + place1 + " " + "with" + " " + person2 + ".");
+System.out.println("Sometimes, they go to the" + " " + adjective2 + " " + place2 + " " + "with" + " " + person2 + " " + "so that they can" + " " + verb3 + " " + "toegther" + ".");
     }
 }
