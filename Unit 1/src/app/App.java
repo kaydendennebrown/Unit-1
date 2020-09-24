b0420a4fe5d03f72db6491a3581d5da7eebb84d7
kaydendennebrown <825058@mystma.org>
2020-09-24 19:49:45 +0000

    September 24th Assignment

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -1,4 +1,5 @@
 package app;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -17,5 +18,24 @@ String adjective2 = "gigantic";
 System.out.println(person1 +" " + "is a programmer who likes to" + " " + verb1 + ".");
 System.out.println("Every"+ " " + dayOfWeek + " " + "they make a" + " " + adjective1 + " " + food + " " + "for brakfast and" + " " + verb2 + " " + "at" + " " + place1 + " " + "with" + " " + person2 + ".");
 System.out.println("Sometimes, they go to the" + " " + adjective2 + " " + place2 + " " + "with" + " " + person2 + " " + "so that they can" + " " + verb3 + " " + "toegther" + ".");
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("Please put your first name");
+String firstName = input.nextLine();
+
+System.out.println("Please put your last name");
+String lastName = input.nextLine();
+
+System.out.println("How old are you");
+String age = input.nextLine();
+
+System.out.println("What grade are you in");
+String grade = input.nextLine();
+
+System.out.println("Please put your favorite color");
+String favColor = input.nextLine();
+
+System.out.println("Hello" + " " + firstName + " " + lastName + "," + "You are in " + grade + " " + "grade" + " and you are " + age + " years old." +" " + "Your favorite color is " + favColor + "." );
     }
 }
