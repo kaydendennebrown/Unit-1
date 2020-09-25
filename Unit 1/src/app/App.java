f7f7fa74bbb43c56326e700c175f5111cce7ccf2
kaydendennebrown <825058@mystma.org>
2020-09-25 18:08:20 +0000

    Madlib Assignment 2

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -3,39 +3,44 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-String person1 = "Cheif Keef";
-String person2 = "Bronny Jr";
-String verb1 = "run";
-String verb2 = "jump";
-String verb3 = "swim";
-String food = "pumpkin pie";
-String dayOfWeek = "Saturday";
-String place1 = "bowling alley";
-String place2 = "Pizza Hut";
-String adjective1 = "sweaty";
-String adjective2 = "gigantic";
-
-System.out.println(person1 +" " + "is a programmer who likes to" + " " + verb1 + ".");
-System.out.println("Every"+ " " + dayOfWeek + " " + "they make a" + " " + adjective1 + " " + food + " " + "for brakfast and" + " " + verb2 + " " + "at" + " " + place1 + " " + "with" + " " + person2 + ".");
-System.out.println("Sometimes, they go to the" + " " + adjective2 + " " + place2 + " " + "with" + " " + person2 + " " + "so that they can" + " " + verb3 + " " + "toegther" + ".");
 
 Scanner input = new Scanner(System.in);
 
-System.out.println("Please put your first name");
-String firstName = input.nextLine();
+System.out.println("Please put a holiday");
+String holiday = input.nextLine();
 
-System.out.println("Please put your last name");
-String lastName = input.nextLine();
+System.out.println("Please put a noun");
+String noun = input.nextLine();
 
-System.out.println("How old are you");
-String age = input.nextLine();
+System.out.println("Please put a place");
+String place = input.nextLine();
 
-System.out.println("What grade are you in");
-String grade = input.nextLine();
+System.out.println("Please put a person");
+String person= input.nextLine();
 
-System.out.println("Please put your favorite color");
-String favColor = input.nextLine();
+System.out.println("Please put an adjective");
+String adjective = input.nextLine();
 
-System.out.println("Hello" + " " + firstName + " " + lastName + "," + " you are in " + grade + " " + "grade" + " and you are " + age + " years old." +" " + "Your favorite color is " + favColor + "." );
+System.out.println("Please put a body part(Plural)");
+String bodypart = input.nextLine();
+
+System.out.println("Please put a verb");
+String verb = input.nextLine();
+
+System.out.println("Please put another adjective");
+String adjective2 = input.nextLine();
+
+System.out.println("Please put another noun");
+String noun2 = input.nextLine();
+
+System.out.println("Please put a food");
+String food = input.nextLine();
+
+System.out.println("Please put a plural noun");
+String pluralNoun = input.nextLine();
+
+System.out.println("I can't believe its already " + holiday + "!" + "I can't wait to put on my " + noun + " and visit every " + place + " in my neighborhood." + " This year, I am going to dress up as " + person + " with " + adjective + " " + bodypart + "." + " Before I " + verb + "," + " I make sure to grab my " + adjective2 + " " + noun2 + " to hold all of my " + food + "." + " Finally, all of my " + pluralNoun + " are ready to go!");
+
+
     }
 }
