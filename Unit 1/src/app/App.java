4a4d7187c89fb3d51c4cf493e0c03551771d7d75
kaydendennebrown <825058@mystma.org>
2020-09-28 19:24:46 +0000

    While Loop Assignment

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -41,6 +41,29 @@ String pluralNoun = input.nextLine();
 
 System.out.println("I can't believe its already " + holiday + "!" + "I can't wait to put on my " + noun + " and visit every " + place + " in my neighborhood." + " This year, I am going to dress up as " + person + " with " + adjective + " " + bodypart + "." + " Before I " + verb + "," + " I make sure to grab my " + adjective2 + " " + noun2 + " to hold all of my " + food + "." + " Finally, all of my " + pluralNoun + " are ready to go!");
 
+int count = 0;
+String pName = "Kayden";
+boolean finished = false;
 
+while (count <= 10) {
+    System.out.println("The count is " + count);
+    count = count + 1;
+}
+
+System.out.println("Count finished");
+
+while (pName == "Kayden") {
+    System.out.println("My name is " + pName);
+    pName = "Jack";
+}
+
+System.out.println("Name finished");
+
+while (finished == false) {
+    System.out.println("Not complete");
+    finished = true;
+}
+
+System.out.println("Complete");
     }
 }
