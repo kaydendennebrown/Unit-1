750a2d8e6fc69b982125463ff79a956aa01e2442
kaydendennebrown <825058@mystma.org>
2020-10-08 22:16:15 +0000

    Cheetah Facts

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -6,29 +6,32 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
-System.out.println("Welcome to Cheetah facts, please input a number (1-5).");
-int v = input.nextInt();
-int a = 1;
+        System.out.println("Welcome to Cheetah facts, please input a number (1-5).");
+        int v = input.nextInt();
 
-        for (v = a; v < 5;)  {
-            if (v == 1) 
-           System.out.println("Cheetahs are the fastest land mammal.");
-           
-           if (v == 2)
-           System.out.println("The scientific name of cheetah is acinonyx jubatus.");
-
-           if (v == 3)
-           System.out.println("Cheetahs live 10-12 years."); 
-
-           if (v == 4)
-           System.out.println("There are only 7,100 cheetahs left alive in the wild.");
-
-           if (v == 5)
-           System.out.println("Cheetahs are the only big cat that can turn midair.");
-
-           if (v > 5)
-           System.out.println("Not a valid number, please enter a number (1-5)");
-       
-         }
+        if (v == 1) {
+            System.out.println("Cheetahs are the fastest land animal.");
         }
+
+        if (v == 2) {
+            System.out.println("The scientific name of cheetah is acinonyx jubatus.");
+        }
+
+        if (v == 3) {
+            System.out.println("Cheetahs live 10-12 years.");
+        }
+
+        if (v == 4) {
+            System.out.println("There are only 7,100 cheetahs left alive in the wild.");
+        }
+
+        if (v == 5) {
+            System.out.println("Cheetahs are the only big cat that can turn midair.");
+        }
+
+        if (v > 5) {
+            System.out.println("Not a valid number, please enter a number (1-5)");
+        }
+
     }
+}
