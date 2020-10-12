0e7c3d9bb79ec64d36a7b0a9e118ce5cef0dc95e
kaydendennebrown <825058@mystma.org>
2020-10-12 20:05:09 +0000

    Challenge 2 Attempt

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -32,9 +32,39 @@ public class App {
         if (v > 5) {
             System.out.println("Not a valid number, please enter a number (1-5)");
         }
-        for (int x = 1; x <= 1000; x = x + 1){
+        for (int x = 1; x <= 1000; x = x + 1) {
             System.out.println(x);
         }
+        String pass = "Pass123";
+        System.out.println("Please input password");
+        
+        String pass1 = input.nextLine();
+
+        if (pass1 == pass) {
+            System.out.println("Password correct, the secret message is apple.");
+        }
+        if (pass1 != pass) {
+            System.out.println("Password incorrect, 2 attempts remaining.");
+        }
+
+        String pass2 = input.nextLine();
+
+        if (pass2 == pass) {
+            System.out.println("Password correct, the secret message is apple.");
+        }
+        if (pass2 != pass) {
+            System.out.println("Password incorrect, 1 attempt remaining.");
+        }
+
+        String pass3 = input.nextLine();
+
+        if (pass3 == pass) {
+            System.out.println("Password correct, the secret message is apple.");
+        }
+        if (pass3 != pass) {
+            System.out.println("Password incorrect, no attempts remaining.");
         }
 
     }
+
+}
